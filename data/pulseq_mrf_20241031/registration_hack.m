5a28eb34a60b31ef72cc90be6404d49b22915fa8
Zhibo Zhu <42652103+zzb0414@users.noreply.github.com>
2025-01-07 11:46:08 -0800

    Updated.

--- data/pulseq_mrf_20241031/registration_hack.m
+++
@@ -1,9 +0,0 @@
-im1 = img_all(:, :, 1);
-im2 = circshift(img_all(:, :, 2), -1, 1);
-im3 = circshift(imrotate(circshift(img_all(:, :, 3), -3, 1), -1, 'crop'), 1, 2);
-im4 = circshift(imrotate(circshift(img_all(:, :, 4), -4, 1), -1, 'crop'), 1, 2);
-
-img_all = cat(3, im1, im2, im3, im4);
-figure;
-imagesc3D(cat(3, im1, im2, im3, im4));
-colormap(gray);
